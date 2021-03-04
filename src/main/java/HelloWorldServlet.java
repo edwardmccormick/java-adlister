d01782c608d60eec5424d24c0b7f1d9414aa0fa4
edwardmccormick <66527737+edwardmccormick@users.noreply.github.com>
2021-03-04 13:46:51 -0600

    servlets exercise complete

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    int count = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("name");
+        response.setContentType("text/html");
+        count++;
+        PrintWriter out = response.getWriter();
+        if (userName.equalsIgnoreCase("reset"))
+        {count = 0; out.println("<h1>Hello, you cheeky monkey! You've discovered how to reset the counter!</h1> <br><p>This page has been viewed " + count + " times...since you reset it!");}
+        else if(userName != null) {
+            out.printf("<h1>Hello, %s!</h1> <br><p>This page has been viewed %d", userName, count);
+        } else {
+            out.printf("<h1>Hello, World!</h1> <br><p>This page has been viewed %d", count);
+        }
 
     }
 }
